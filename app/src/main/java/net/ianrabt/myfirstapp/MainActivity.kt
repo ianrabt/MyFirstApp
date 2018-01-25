afc9266207120f995fe616da932082aeba06ff34
Ian Taylor <itaylor@hmc.edu>
2018-01-24 23:26:45 -0800

    first commit

---
+++ app/src/main/java/net/ianrabt/myfirstapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.ianrabt.myfirstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+
+    }
+}

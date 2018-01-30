51529ff73b5e387c21df41380231a17fb3a664b9
Ian Taylor <itaylor@hmc.edu>
2018-01-30 11:31:51 -0800

    react to user inputted text

--- app/src/main/java/net/ianrabt/myfirstapp/MainActivity.kt
+++ app/src/main/java/net/ianrabt/myfirstapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package net.ianrabt.myfirstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "net.ianrabt.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,12 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString();
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
 
+        startActivity(intent)
     }
 }
